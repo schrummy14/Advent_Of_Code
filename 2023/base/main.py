98365815e24c2192e97fec980952f4cbdd70537d
schrummy14 <schrummy14@gmail.com>
2023-12-07 11:04:51 -0600

    Added template

---
+++ 2023/base/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+def getData_1(filename:str):
+    pass
+
+def getData_2(filename:str):
+    pass
+
+def doPart_1(data):
+    pass
+
+def doPart_2(data):
+    pass
+
+def main():
+    # filename = 'example.dat'
+    filename = 'input.dat'
+    answer = doPart_1(getData_1(filename))
+    print(answer)
+    answer = doPart_2(getData_2(filename))
+    print(answer)
+
+if __name__ == "__main__":
+    main()
