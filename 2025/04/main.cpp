3a4e527460a890f204eeaf7ef3e537040c93dfc3
Matthew Schramm <schrummy14@gmail.com>
2025-12-04 15:51:57 -0600

    2025 Day 4 Part 2

--- 2025/04/main.cpp
+++ 2025/04/main.cpp
@@ -10,6 +10,7 @@ void doPart1(const char* filename)
         stripString(line);
         GRID.push_back(line);
     }
+    file.close();
     int R = GRID.size();
     int C = GRID[0].length();
     size_t ans = 0;
@@ -26,10 +27,9 @@ void doPart1(const char* filename)
                     if (GRID[nr][nc] == '@') numNeighbors++;
                 }
             }
-            if (numNeighbors <= 4) ans++;
+            if (numNeighbors <= 4) ans++; // <= 4 because we are counting itself...
         }
     }
-    file.close();
     std::cout << ans << '\n';
 }
 
@@ -37,11 +37,40 @@ void doPart2(const char* filename)
 {
     std::ifstream file(filename);
     std::string line;
+    std::vector<std::string> GRID;
     while (getline(file, line)) {
         stripString(line);
-        std::vector<std::string> sLine = splitString(line, " ");
+        GRID.push_back(line);
     }
     file.close();
+    int R = GRID.size();
+    int C = GRID[0].length();
+    size_t ans = 0;
+    bool done = false;
+    while (!done) {
+        done = true;
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                if (GRID[r][c] != '@') continue;
+                int numNeighbors = 0;
+                for (int dr = -1; dr <= 1; dr++) {
+                    const int nr = r + dr;
+                    if (nr < 0 || nr >= R) continue;
+                    for (int dc = -1; dc <= 1; dc++) {
+                        const int nc = c + dc;
+                        if (nc < 0 || nc >= C) continue;
+                        if (GRID[nr][nc] == '@') numNeighbors++;
+                    }
+                }
+                if (numNeighbors <= 4) { // <= 4 because we are counting itself...
+                    ans++;
+                    GRID[r][c] = 'x';
+                    done = false;
+                }
+            }
+        }
+    }
+    std::cout << ans << '\n';
 }
 
 int main(int narg, char* args[])
