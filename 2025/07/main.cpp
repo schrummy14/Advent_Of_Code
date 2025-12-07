49a8f234c39f62195e072b965a87053f1889817c
schrummy14 <schrummy14@gmail.com>
2025-12-07 16:16:40 -0600

    Part 2 Day 7 2025

--- 2025/07/main.cpp
+++ 2025/07/main.cpp
@@ -34,15 +34,56 @@ void doPart1(const char* filename)
     std::cout << "Part 1: " << ans << '\n';
 }
 
+size_t shootRay(size_t r, size_t c, std::vector<std::string>& GRID, std::vector<size_t>& SEEN)
+{
+    static const size_t R = GRID.size();
+    static const size_t C = GRID[0].length();
+
+    if (SEEN[r*C + c] > 0) return SEEN[r*C+c];
+
+    if (r == R) return 1;
+
+    size_t ans = 0;
+    if (r+1 < R && GRID[r+1][c] == '^'){
+        ans+=shootRay(r+1, c-1, GRID, SEEN);
+        ans+=shootRay(r+1, c+1, GRID, SEEN);
+    } else {
+        ans+=shootRay(r+1, c+0, GRID, SEEN);
+    }
+    SEEN[r*C + c] = ans;
+    return ans;
+}
+
 void doPart2(const char* filename)
 {
     std::ifstream file(filename);
     std::string line;
+    std::vector<std::string> GRID;
     while (getline(file, line)) {
         stripString(line);
-        std::vector<std::string> sLine = splitString(line, " ");
+        GRID.push_back(line);
     }
     file.close();
+
+    const size_t R = GRID.size();
+    const size_t C = GRID[0].length();
+    size_t curC = 0;
+    for (size_t c = 0; c < C; c++) {
+        if (GRID[0][c] == 'S') {
+            curC = c;
+            break;
+        }
+    }
+    GRID[1][curC] = '|';
+    std::vector<size_t> SEEN;
+    for (size_t r = 0; r < R; r++) {
+        for (size_t c = 0; c < C; c++) {
+            SEEN.push_back(0);
+        }
+    }
+
+    size_t ans = shootRay(1, curC, GRID, SEEN);
+    std::cout << "Part 2: " << ans << '\n';
 }
 
 int main(int narg, char* args[])
